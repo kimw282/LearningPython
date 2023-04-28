897c11a35698a5786f71d7c28a136a7fc1ee0849
Kimberley Wong <kimberley@Kimberleys-MacBook-Air.local>
2023-04-29 09:41:44 +1000

    Added help, Fixec bugs

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,15 +4,37 @@ row3 = [" ", "|", " ", "|", " "]
 
 tic_tac_toe = [row1, row2, row3]
 
+key = '''
+Cell Guide:
+
+0 0 | 1 0 | 2 0
+---------------
+0 1 | 1 1 | 2 1
+---------------
+0 2 | 1 2 | 2 2
+
+    '''
+
 
 i = 0
 stop_flag = False
+print(key)
 while not stop_flag:
-    player_input = input("Input [x y] coordinates in the range [0-2]: ").split()
+    player_input = input("Input [x y] coordinates or \"help\": ").split()
     n = 0
     while n < len(player_input):
-        player_input[n] = int(player_input[n])
+        try:
+            player_input[n] = int(player_input[n])
+        except ValueError:
+            break
         n += 1
+
+    if isinstance(player_input[0], str):
+        if player_input[0].lower() == "help":
+            print(key)
+            continue
+        if player_input[0].lower() == "quit":
+            exit()
 
     if i % 2 == 0:
         if tic_tac_toe[player_input[1]][player_input[0]*2] == " ":
@@ -44,12 +66,10 @@ while not stop_flag:
             print(f"\n{tic_tac_toe[0][4]} wins!")
             stop_flag = True
             break
-        elif ((" " not in tic_tac_toe[0]) and (" " not in tic_tac_toe[1]) and 
-              (" " not in tic_tac_toe[2])):
-            print("\nDraw")
-            stop_flag = True
-            break
         n += 1
+    if ((" " not in tic_tac_toe[0]) and (" " not in tic_tac_toe[1]) and 
+              (" " not in tic_tac_toe[2])) and not stop_flag:
+            print("\nDraw")
     i += 1
     tic_tac_toe_joined = [''.join(row1), ''.join(row2), ''.join(row3)]
     print("\n", ''.join(tic_tac_toe_joined), "\n", sep='')
